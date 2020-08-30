2ccd8a01c9d7c12966ad8540378f500c8ce568d4
Przemek <przemekmachura94@gmail.com>
2020-08-30 12:33:48 +0200

    Old Html tags extraction approach removed (replaced by JSOUP)

--- information-manager-domain/src/main/java/machura/przemyslaw/informationmanagerdomain/domain/tags/extraction/fromtext/deprecated/TagAttributesBuilder.java
+++
@@ -1,9 +0,0 @@
-package machura.przemyslaw.informationmanagerdomain.domain.tags.extraction.fromtext.deprecated;
-
-import io.vavr.control.Either;
-import machura.przemyslaw.informationmanagerdomain.domain.errors.Fault;
-import machura.przemyslaw.informationmanagerdomain.domain.tags.TagAttr;
-
-public interface TagAttributesBuilder {
-     Either<Fault, TagAttr> build(String name, String value);
-}
